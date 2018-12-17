b47880fc02165d7cb0da27fc6fdcea111f73b26a
joaquin3684 <joaquinmazoud@gmail.com>
2018-12-17 16:00:31 -0300

    lkjasdlkfj

--- app/repositories/VentaRepository.scala
+++ app/repositories/VentaRepository.scala
@@ -66,6 +66,10 @@ object VentaRepository extends Estados {
   def modificarVenta(venta: Venta, idVenta:Long, user: String, fechaCreacion: DateTime) = {
 
     val updateV = ventas.filter(_.id === idVenta).map( x => (x.dni, x.nombre, x.nacionalidad, x.domicilio, x.localidad, x.telefono, x.cuil, x.estadoCivil, x.edad, x.idObraSocial, x.fechaNacimiento, x.zona, x.codigoPostal, x.horaContactoTel, x.piso, x.dpto, x.celular, x.horaContactoCel, x.base, x.empresa, x.cuit)).update((venta.dni, venta.nombre, venta.nacionalidad, venta.domicilio, venta.localidad, venta.telefono, venta.cuil, venta.estadoCivil, venta.edad, venta.idObraSocial, venta.fechaNacimiento, venta.zona, venta.codigoPostal, venta.horaContactoTel, venta.piso, venta.dpto, venta.celular, venta.horaContactoCel, venta.base, venta.empresa, venta.cuit))
-    Db.db.run(updateV)
+    val estado = estados.filter(x => x.id === idVenta && x.estado === CREADO).map( x => (x.user, x.fecha)).update((user, fechaCreacion))
+
+    val fullQuery = DBIO.seq(updateV, estado)
+    Db.db.run(fullQuery.transactionally)
+
   }
 }
